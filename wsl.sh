57711deeafc6a6b8e6371ff9a2dfae8ce894eec6
eldoranstars <igor.dorozhkin@nedra.digital>
2022-03-28 00:20:32 +0300

    test

--- wsl.sh
+++ wsl.sh
@@ -2,7 +2,7 @@
 # https://github.com/ohmybash/oh-my-bash
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 sed -i 's/OSH_THEME="font"/OSH_THEME="powerline"/g' ~/.bashrc
-sed -i 's/_CLEAN_COLOR=25/_CLEAN_COLOR=6/g' ~/.oh-my-bash/themes/powerline/powerline.theme.sh
+sed -i 's/_CLEAN_COLOR=25/_CLEAN_COLOR=16/g' ~/.oh-my-bash/themes/powerline/powerline.theme.sh
 sed -i 's/_DIRTY_COLOR=88/_DIRTY_COLOR=4/g' ~/.oh-my-bash/themes/powerline/powerline.theme.sh
 sed -i 's/_STAGED_COLOR=30/_STAGED_COLOR=3/g' ~/.oh-my-bash/themes/powerline/powerline.theme.sh
 sed -i 's/_UNSTAGED_COLOR=92/_UNSTAGED_COLOR=1/g' ~/.oh-my-bash/themes/powerline/powerline.theme.sh
